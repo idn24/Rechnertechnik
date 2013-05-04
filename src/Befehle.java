0e890cf29e9d33eb336b709293c7e43f293d3391
idN24 <death_eye@web.de>
2013-05-04 13:36:42 +0200

    init Laptop

---
+++ src/Befehle.java
@@ -0,0 +1,77 @@
+
+public class Befehle {
+	
+	public void setBit(int opcode){
+		
+	}
+	
+	public void clr(int opcode){
+		
+	}
+	
+	public void andwf(int opcode){
+		
+	}
+	
+	public void or(int opcode){
+		
+	}
+	
+	public void addwf(int opcode){
+		
+	}
+	
+	public void comf(int opcode){
+	
+	}
+	public void decf(int opcode){
+	
+	}
+	public void decfsz(int opcode){
+	
+	}
+	
+	public void incf(int opcode){
+		
+	}
+	
+	public void incfsz(int opcode){
+		
+	}
+	
+	public void iorwf(int opcode){
+		
+	}
+	
+	public void movf(int opcode){
+		
+	}
+	
+	public void movwf(int opcode){
+		
+	}
+	
+	public void nop(int opcode){
+		
+	}
+	
+	public void rlf(int opcode){
+		
+	}
+	
+	public void rrf(int opcode){
+		
+	}
+	
+	public void subwf(int opcode){
+		
+	}
+	
+	public void swapf(int opcode){
+		
+	}
+	
+	public void xorwf(int opcode){
+		
+	}
+}
--- src/Steuerung.java
+++ src/Steuerung.java
@@ -10,9 +10,12 @@ public class Steuerung {
 	DateiEinlesen read;
 	Programm programm;
 	FileChooser chooser;
+	private Befehle befehle;
+	int programcounter = 0;
 	
 	public Steuerung (){
 		gui = new Oberflaeche(this);
+		befehle = new Befehle();
 	}
 	
 	public void oeffneDatei(){
@@ -28,5 +31,67 @@ public class Steuerung {
 			gui.setZeile(programm.getDatei().get(i));
 		}
 	}
+	
+	public int run(){
+		boolean run = true;
+		int opcode =0;
+		while (run){
+			
+			if (opcode <= 2047 && opcode >= 1792){
+				befehle.addwf(opcode);
+			}
+			else if( opcode <= 1535 && opcode >=1280){
+				befehle.andwf(opcode);
+			}
+			else if( opcode <= 511 && opcode >=256){
+				befehle.clr(opcode);
+			}
+			else if( opcode <= 2559 && opcode >=2304){
+				befehle.comf(opcode);
+			}
+			else if( opcode <= 1023 && opcode >=768){
+				befehle.decf(opcode);
+			}
+			else if( opcode <= 3070 && opcode >=2816){
+				befehle.decfsz(opcode);
+			}
+			else if( opcode <= 2815 && opcode >=2560){
+				befehle.incf(opcode);
+			}
+			else if( opcode <= 4095 && opcode >=3840){
+				befehle.incfsz(opcode);
+			}
+			else if( opcode <=1279 && opcode >=1024){
+				befehle.iorwf(opcode);
+			}
+			else if( opcode <= 0x08FF && opcode >=0x0800){
+				befehle.movf(opcode);
+			}
+			else if( opcode <= 0x00FF && opcode >=0x0080){
+				befehle.movwf(opcode);
+			}
+			else if( opcode <= 0x0060 && opcode >=0x0000){
+				befehle.nop(opcode);
+			}
+			else if( opcode <= 0x0dff && opcode >=0x0d00){
+				befehle.rlf(opcode);
+			}
+			else if( opcode <= 0x0cff && opcode >=0x0c00){
+				befehle.rrf(opcode);
+			}
+			else if( opcode <= 0x02ff && opcode >=0x0200){
+				befehle.subwf(opcode);
+			}
+			else if( opcode <= 0x0eff && opcode >=0x0e00){
+				befehle.swapf(opcode);
+			}
+			else if( opcode <= 0x06ff && opcode >=0x0600){
+				befehle.xorwf(opcode);
+			}
+		}
+		
+		return 0;
+	}
+	
 
 }
